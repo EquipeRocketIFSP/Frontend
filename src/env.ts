76e2823b77d2e718612cf87c81d294092dc88d27
hshima <22624182+hshima@users.noreply.github.com>
2022-11-22 15:26:49 -0300

    Altera endpoint de acesso

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,3 @@
 export default {
-    API: window.origin.match("localhost") ? "http://localhost:8080" : "http://backend-poc.us-east-1.elasticbeanstalk.com:8080",
+    API: window.origin.match("localhost") ? "http://localhost:8080" : "http://mvp-certvet.us-east-1.elasticbeanstalk.com:8080",
 };
